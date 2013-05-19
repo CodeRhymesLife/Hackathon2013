79b3134917090682106546217feeeba5ad8843e2
rcjames1004 <rcjames1004@gmail.com>
2013-05-18 20:45:24 -0700

    Serve sends to client
    Boom

---
+++ server/websocketserver.js
@@ -0,0 +1,28 @@
+﻿var sys = require("sys");
+var WebSocketServer = require('ws').Server
+  , wss = new WebSocketServer({ port: 8080 });
+
+var clients = [];
+wss.on('connection', function (ws) {
+    sys.debug("connected to client");
+    clients.push(ws);
+
+    ws.on('message', function (message) {
+        sys.debug("recieved data");
+        console.log('received message: %s', message);
+
+        // handle incoming data
+        // send data to ALL clients whenever ANY client send up data
+        for (var i = 0 ; i < clients.length ; i++) {
+            clients[i].send(message);
+        }
+    });
+
+    ws.on("close", function () {
+        debugger;
+        // emitted when server or client closes connection
+        sys.debug("close");
+    });
+
+    ws.send('something');
+});
